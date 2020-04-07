10ecd6bb807d2e7e897183e5c23165e0d68d2cd2
jequezinho <jroverby92@gmail.com>
2020-04-07 14:00:25 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
-// Your code here
+function saturdayFun(str="roller-skate") {
+    return `This Saturday, I want to ${str}!`;
+}
+
+function mondayWork(str="go to the office") {
+    return `This Monday, I will ${str}.`;
+}
+
+function wrapAdjective(rap="*") {
+    return function(str="special") {
+        return `You are ${rap}${str}${rap}!`;
+    }
+}
+
+const Calculator = {
+    "subtract": function(num1, num2) {
+        return num1 - num2;
+    },
+    "add": function(num1, num2) {
+        return num1 + num2;
+    },
+    "multiply": function(num1, num2) {
+        return num1 * num2;
+    },
+    "divide": function(num1, num2) {
+        return num1 / num2;
+    }
+}
+
+function actionApplyer(stInt, fnArray) {
+    for (const fn of fnArray) {
+        stInt = fn(stInt);
+    }
+    return stInt;
+}
