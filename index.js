24f2bb78e88ffbe8066ed3f170bcfa1b25de9a7b
ellisandrews <ellisandrews1@gmail.com>
2020-04-08 11:39:08 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,44 @@
-// Your code here
+const saturdayFun = (activity = 'roller-skate') => `This Saturday, I want to ${activity}!`;
+
+const mondayWork = (activity='go to the office') => `This Monday, I will ${activity}.`;
+
+const wrapAdjective = (flair='*') => {
+    return (adj='special') => `You are ${flair}${adj}${flair}!` 
+}
+
+// Option 1
+class CalculatorClass {
+    add(x, y) {
+        return x + y
+    }
+
+    subtract(x, y) {
+        return x - y
+    }
+
+    multiply(x, y) {
+        return x * y
+    }
+
+    divide(x, y) {
+        return x / y
+    }
+}
+
+// const Calculator = new CalculatorClass();
+
+// Option 2
+const Calculator = {
+    add: (x, y) => x + y,
+    subtract: (x, y) => x - y,
+    multiply: (x, y) => x * y,
+    divide: (x, y) => x / y
+}
+
+
+const actionApplyer = (startInt, funcArray) => {
+    funcArray.forEach(func => {
+        startInt = func(startInt);  // Apply the function
+    });
+    return startInt
+}
