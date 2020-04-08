149cafbb1452ad31433dd34efb62205e81a3f34f
cooljasonmelton <cooljasonmelton>
2020-04-08 18:52:54 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
-// Your code here
+const saturdayFun = (activity = 'roller-skate') => {
+    return `This Saturday, I want to ${activity}!`
+}
+
+const mondayWork = (activity = 'go to the office') => {
+    return `This Monday, I will ${activity}.`
+}
+
+const wrapAdjective = (string = '*') => {
+    return (input = 'special') => {
+        return `You are ${string}${input}${string}!`
+    }
+}
+
+const Calculator = {
+    add: (x, y) => x + y,
+    subtract: (x, y) => x - y, 
+    multiply: (x, y) => x * y,
+    divide: (x, y) => x / y  
+}
+
+const actionApplyer = (x, array) => {
+    if (array.length === 0) {
+        return x 
+    }
+
+    // x = array[0](x)
+    // x = array[1](x)
+    // x = array[2](x)
+    x;
+    for (let i = 0; i < array.length; i++) {
+        x = array[i](x)
+    }
+
+    return x 
+  
+}
